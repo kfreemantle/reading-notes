9ad02700a57cc35b346ffdf0262a61d516d5b315
Kyle Freemantle <kcfreemantle@gmail.com>
2023-01-11 16:55:04 -0800

    First Javascript update to the Pixel site.

---
+++ code-102/Lab04/index.js
@@ -0,0 +1,18 @@
+//basic definitions
+
+const catColor = 'black';
+let userGuess = prompt('What color is Pixel?');
+    console.log("User guessed: ") +userGuess);
+    
+//conditional logic
+
+if (userGuess == catColor) {
+    console.log("Heck yes!");
+})else if (userGuess == "orange") {
+    console.log("No, though he is about as bright.");
+}
+    )else if (userGuess == "Black") {
+        console.log("Yes, but with bad capitalization.");
+} else {
+    console.log('No, that is not correct.');
+}
